d6f99314c8f890328afdf5a3dc19122fe911f8af
Tkng <13506491+shsw228@users.noreply.github.com>
2024-10-01 18:52:33 +0900

    Initial Commit

---
+++ Quiz-App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz-App
+//  
+//  Created by shsw228 on 2024/10/01
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Quiz-AppTests/Quiz_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Quiz_AppTests.swift
+//  Quiz-AppTests
+//  
+//  Created by shsw228 on 2024/10/01
+//
+
+import Testing
+
+struct Quiz_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
